d34e11d0477749e720fe0bec73bc39d362f01c9c
Pete Shearer <peter.shearer@gmail.com>
2014-02-16 22:48:02 -0500

    Intro to AOP 3 Blog Post added - Background Thread Execution

---
+++ IntroToAop3/IntroToAop3/LongRunningClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using PostSharp.Patterns.Threading;
+
+namespace IntroToAop3
+{
+    public class LongRunningClass
+    {
+        public void LongRunningOperationBlocking()
+        {
+            Thread.Sleep(3000);
+            return;
+        }
+
+        [Background]
+        public void LongRunningOperationBackground()
+        {
+            Thread.Sleep(3000);
+            return;
+        }
+    }
+}
---
+++ IntroToAop3/IntroToAop3/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IntroToAop3
+{
+    public partial class MainWindow : Window
+    {
+        protected LongRunningClass _longRunningClass;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _longRunningClass = new LongRunningClass();
+        }
+
+        private void btnBreakIt_Click(object sender, RoutedEventArgs e)
+        {
+            _longRunningClass.LongRunningOperationBlocking();
+            MessageBox.Show(txtBreakIt.Text);
+        }
+
+        private void btnDontBreakIt_Click(object sender, RoutedEventArgs e)
+        {
+            _longRunningClass.LongRunningOperationBackground();
+            MessageBox.Show(txtDontBreakIt.Text);
+        }
+    }
+}
